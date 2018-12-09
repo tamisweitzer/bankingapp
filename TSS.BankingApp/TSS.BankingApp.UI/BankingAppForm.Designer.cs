b9042ee9fdd96bde72d0509f008c703e4c80e8c0
tamara0715@gmail.com <tamara0715@gmail.com>
2018-12-08 21:40:27 -0600

    Initial commit

---
+++ TSS.BankingApp/TSS.BankingApp.UI/BankingAppForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace TSS.BankingApp.UI
+{
+    partial class BankingAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxCustomersList = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblDisplayID = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblSSN = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtSSN = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblDisplayAge = new System.Windows.Forms.Label();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstbxCustomersList
+            // 
+            this.lstbxCustomersList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbxCustomersList.FormattingEnabled = true;
+            this.lstbxCustomersList.ItemHeight = 33;
+            this.lstbxCustomersList.Location = new System.Drawing.Point(49, 38);
+            this.lstbxCustomersList.Name = "lstbxCustomersList";
+            this.lstbxCustomersList.Size = new System.Drawing.Size(527, 499);
+            this.lstbxCustomersList.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 566);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1116, 258);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(49, 846);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(1116, 255);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // lblID
+            // 
+            this.lblID.BackColor = System.Drawing.SystemColors.Control;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(599, 37);
+            this.lblID.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(227, 44);
+            this.lblID.TabIndex = 3;
+            this.lblID.Text = "ID";
+            this.lblID.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblDisplayID
+            // 
+            this.lblDisplayID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayID.Location = new System.Drawing.Point(865, 37);
+            this.lblDisplayID.Name = "lblDisplayID";
+            this.lblDisplayID.Size = new System.Drawing.Size(300, 44);
+            this.lblDisplayID.TabIndex = 4;
+            this.lblDisplayID.Text = "..................";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(599, 109);
+            this.lblFirstName.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(227, 44);
+            this.lblFirstName.TabIndex = 5;
+            this.lblFirstName.Text = "First Name";
+            this.lblFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(599, 181);
+            this.lblLastName.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(227, 44);
+            this.lblLastName.TabIndex = 6;
+            this.lblLastName.Text = "Last Name";
+            this.lblLastName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblSSN
+            // 
+            this.lblSSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSSN.Location = new System.Drawing.Point(599, 253);
+            this.lblSSN.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblSSN.Name = "lblSSN";
+            this.lblSSN.Size = new System.Drawing.Size(227, 44);
+            this.lblSSN.TabIndex = 7;
+            this.lblSSN.Text = "SSN";
+            this.lblSSN.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthDate.Location = new System.Drawing.Point(599, 325);
+            this.lblBirthDate.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(227, 44);
+            this.lblBirthDate.TabIndex = 8;
+            this.lblBirthDate.Text = "Birth Date";
+            this.lblBirthDate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(865, 109);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(300, 40);
+            this.txtFirstName.TabIndex = 9;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(865, 181);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(300, 40);
+            this.txtLastName.TabIndex = 10;
+            // 
+            // txtSSN
+            // 
+            this.txtSSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSSN.Location = new System.Drawing.Point(865, 253);
+            this.txtSSN.Name = "txtSSN";
+            this.txtSSN.Size = new System.Drawing.Size(300, 40);
+            this.txtSSN.TabIndex = 11;
+            // 
+            // lblAge
+            // 
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(599, 397);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(233, 44);
+            this.lblAge.TabIndex = 12;
+            this.lblAge.Text = "Age";
+            this.lblAge.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblDisplayAge
+            // 
+            this.lblDisplayAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayAge.Location = new System.Drawing.Point(865, 397);
+            this.lblDisplayAge.Name = "lblDisplayAge";
+            this.lblDisplayAge.Size = new System.Drawing.Size(300, 44);
+            this.lblDisplayAge.TabIndex = 13;
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBirthDate.Location = new System.Drawing.Point(865, 325);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(300, 40);
+            this.txtBirthDate.TabIndex = 14;
+            // 
+            // BankingAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 1113);
+            this.Controls.Add(this.txtBirthDate);
+            this.Controls.Add(this.lblDisplayAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtSSN);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblSSN);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblDisplayID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lstbxCustomersList);
+            this.Name = "BankingAppForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TSS.BankingApp";
+            this.Load += new System.EventHandler(this.BankingAppForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxCustomersList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblDisplayID;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblSSN;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtSSN;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblDisplayAge;
+        private System.Windows.Forms.TextBox txtBirthDate;
+    }
+}
+
---
+++ TSS.BankingApp/TSS.BankingApp.UI/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSS.BankingApp.UI
+{
+    class Person { 
+    private string SSN;
+    private string FirstName;
+    private string LastName;
+    private string FullName;
+    private int Age;
+    private DateTime BirthDate;
+    }
+
+    
+}
